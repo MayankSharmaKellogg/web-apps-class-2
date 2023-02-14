69d48190b831f5d862d646c9d1646f8b43b829dd
MayankSharmaKellogg <mayank.sharma2@kellogg.northwestern.edu>
2023-02-14 20:38:48 +0000

    first

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,12 @@
 class CompaniesController < ApplicationController
 
   # TODO:
-  # def index
-  # end
-
-  def show
-    @company = Company.find_by({ "id" => params["id"] })
+  def index
+    @companies = Company.all
   end
+
+  #def show
+  #  @company = Company.find_by({ "id" => params["id"] })
+  #end
 
 end
